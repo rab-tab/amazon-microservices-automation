08515cf059422664c7bd79ea6b006f3b5d9060f4
Rabia <sdetcode@gmail.com>
2026-08-18 18:14:23 +0530

    refactor - replace improper builder with poper builder pattern in RequestSpecificationOptions

--- src/main/java/com/amazon/tests/config/restAsssured/RequestSpecificationOptions.java
+++ src/main/java/com/amazon/tests/config/restAsssured/RequestSpecificationOptions.java
@@ -1,10 +1,12 @@
 package com.amazon.tests.config.restAsssured;
 
+import lombok.Builder;
 import lombok.Getter;
 
 import java.util.Map;
 
 @Getter
+@Builder
 public class RequestSpecificationOptions {
     private String token;
     private Map<String, String> headers;
@@ -15,17 +17,4 @@ public class RequestSpecificationOptions {
     private Long connectionTimeout;
     private Long socketTimeout;
 
-    public static RequestSpecificationOptions builder() { return new RequestSpecificationOptions(); }
-
-    public RequestSpecificationOptions token(String token) { this.token = token; return this; }
-    public RequestSpecificationOptions headers(Map<String, String> headers) { this.headers = headers; return this; }
-    public RequestSpecificationOptions queryParams(Map<String, ?> queryParams) { this.queryParams = queryParams; return this; }
-    public RequestSpecificationOptions pathParams(Map<String, ?> pathParams) { this.pathParams = pathParams; return this; }
-    public RequestSpecificationOptions body(Object body) { this.body = body; return this; }
-    public RequestSpecificationOptions cookies(Map<String, ?> cookies) { this.cookies = cookies; return this; }
-    public RequestSpecificationOptions timeouts(long connectionTimeout, long socketTimeout) {
-        this.connectionTimeout = connectionTimeout;
-        this.socketTimeout = socketTimeout;
-        return this;
-    }
 }
